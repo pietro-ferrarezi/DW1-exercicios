e560642bb16a89e2dfef5d90daabde59aa85ccc4
pietro-ferrarezi <sattu.santitt@gmail.com>
2026-03-26 10:48:37 -0300

    Melhorias

--- Exercicio 01/public/script.js
+++ Exercicio 01/public/script.js
@@ -2,12 +2,17 @@ function calcBhaskara() {
     let a = document.querySelector("#A").value
     let b = document.querySelector("#B").value
     let c = document.querySelector("#C").value
- 
-    let delta = Math.sqrt((b**2) - (4 * a * c))
+    
+    let calc_delta = (b**2) - (4 * a * c)
+    if (calc_delta < 0) {
+        return alert("Delta é negativo, não existe raiz real")
+    }
+
+    let delta = Math.sqrt(calc_delta)
 
     let x1 = (-(b) + delta) / (2 * a)
     let x2 = (-(b) - delta) / (2 * a)
 
-    document.querySelector("#x1").innerText = Math.round(x1)
-    document.querySelector("#x2").innerText = Math.round(x2)
+    document.querySelector("#x1").innerText = x1.toFixed(4)
+    document.querySelector("#x2").innerText = x2.toFixed(4)
 }
