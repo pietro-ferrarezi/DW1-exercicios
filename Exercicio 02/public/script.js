881ab72d3d1e981d889ca5bc334323b22ab83546
pietro-ferrarezi <sattu.santitt@gmail.com>
2026-03-26 11:51:23 -0300

    Exercicio 02

---
+++ Exercicio 02/public/script.js
@@ -0,0 +1,33 @@
+function calcular() {
+    const peso = document.querySelector("#kg").value 
+    const altura = document.querySelector("#m").value
+
+    if (!peso || !altura) {
+        return alert("Adicione todas as informações!")
+    }
+
+    if (!peso > 0 || !altura > 0) {
+        return alert("Dados inválidos")
+    }
+
+    let imc = (peso / (altura ** 2)).toFixed(1)
+
+    document.querySelector("#result-number").innerText = imc
+
+    let analise = ""
+    if (imc < 18.5) {
+        analise = "Abaixo do peso"
+    } else if (imc < 24.5) {
+        analise = "Peso normal"
+    } else if (imc < 29.9) {
+        analise = "Sobrepeso"
+    } else if (imc < 34.9) {
+        analise = "Obesidade 1"
+    } else if (imc < 39.9) {
+        analise = "Obesidade 2"
+    } else if (imc > 40) {
+        analise = "Obesidade 3"
+    }
+
+    document.querySelector("#result-text").innerText = analise
+}
