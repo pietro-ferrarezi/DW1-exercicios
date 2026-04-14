a05bb072f54be7c4f0f304e100281c112a01f0b4
pietro-ferrarezi <sattu.santitt@gmail.com>
2026-04-14 11:49:09 -0300

    Exercicio 08 - 0.2

--- Exercicio 08 - Dogão/public/script.js
+++ Exercicio 08 - Dogão/public/script.js
@@ -1,22 +1,47 @@
 let quantidades = [[22, 0], [26, 0], [29, 0], [5, 0], [8, 0]]
 
 document.querySelector("#cqb-q").addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[eE]/g, '')
+    if (event.target.value < 0) {
+        event.target.value = 0
+        return
+    }
     quantidades[0][1] = event.target.value
     increment_total()
 })
 document.querySelector("#cqd-q").addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[eE]/g, '')
+    if (event.target.value < 0) {
+        event.target.value = 0
+        return
+    }
     quantidades[1][1] = event.target.value
     increment_total()
 })
 document.querySelector("#xs-q").addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[eE]/g, '')
+    if (event.target.value < 0) {
+        event.target.value = 0
+        return
+    }
     quantidades[2][1] = event.target.value
     increment_total()
 })
 document.querySelector("#r350-q").addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[eE]/g, '')
+    if (event.target.value < 0) {
+        event.target.value = 0
+        return
+    }
     quantidades[3][1] = event.target.value
     increment_total()
 })
 document.querySelector("#r1-q").addEventListener('input', (event) => {
+    event.target.value = event.target.value.replace(/[eE]/g, '')
+    if (event.target.value < 0) {
+        event.target.value = 0
+        return
+    }
     quantidades[4][1] = event.target.value
     increment_total()
 })
