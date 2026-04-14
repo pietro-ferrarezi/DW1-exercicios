9fff2bbbb15e9ad9821999db31a65dcb41b2ff77
pietro-ferrarezi <sattu.santitt@gmail.com>
2026-04-14 11:37:12 -0300

    Exercicio 08

---
+++ Exercicio 08 - Dogão/public/script.js
@@ -0,0 +1,33 @@
+let quantidades = [[22, 0], [26, 0], [29, 0], [5, 0], [8, 0]]
+
+document.querySelector("#cqb-q").addEventListener('input', (event) => {
+    quantidades[0][1] = event.target.value
+    increment_total()
+})
+document.querySelector("#cqd-q").addEventListener('input', (event) => {
+    quantidades[1][1] = event.target.value
+    increment_total()
+})
+document.querySelector("#xs-q").addEventListener('input', (event) => {
+    quantidades[2][1] = event.target.value
+    increment_total()
+})
+document.querySelector("#r350-q").addEventListener('input', (event) => {
+    quantidades[3][1] = event.target.value
+    increment_total()
+})
+document.querySelector("#r1-q").addEventListener('input', (event) => {
+    quantidades[4][1] = event.target.value
+    increment_total()
+})
+
+function increment_total() {
+    let total = 0
+    quantidades.forEach((i, index) => {
+        total += quantidades[index][0] * quantidades[index][1]
+        console.log(quantidades[index][0])
+        console.log(quantidades[index][1])
+        console.log(total)
+    })
+    document.querySelector("#total").innerText = total.toFixed(2)
+}
